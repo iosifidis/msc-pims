5c62f68a358883d493c9f560a36753fa31564906
iosifidis <eiosifidis@gmail.com>
2026-01-24 12:59:44 +0200

    Deploy on server

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -59,7 +59,7 @@ const ProfilePage = () => {
             }
 
             // Assuming endpoint is PUT /api/users/{id}
-            await api.put(`http://localhost:8080/api/users/${user.id}`, payload, {
+            await api.put(`/users/${user.id}`, payload, {
                 headers: { Authorization: `Bearer ${token}` }
             });
 
@@ -84,7 +84,7 @@ const ProfilePage = () => {
         }
 
         try {
-            await api.delete(`http://localhost:8080/api/users/${user.id}`, {
+            await api.delete(`/users/${user.id}`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
             alert("Account deleted.");
